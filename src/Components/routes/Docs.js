8162d0833456bab3dab0e33631a834ad89c17866
Haseeb Mohammed <you@example.com>
2022-08-18 18:53:46 +0100

    first

--- src/Components/routes/Docs.js
+++ src/Components/routes/Docs.js
@@ -11,7 +11,7 @@ function Docs() {
                 <p>If you want to know what is ethereum, how it works, and what it can be used for, without going deep into the technical abyss, this guide is perfect for you.</p>
                 <h2>Quick start</h2>
                 <h3>More info on quick start</h3>
-                <p>To interact with a Aire cluster, we will use its command-line interface, also known as the CLI. We use the command-line because it is the first place the AireCoin core team deploys new functionality. The command-line interface is not necessarily the easiest to use, but it provides the most direct, flexible, and secure access to your Solana accounts.</p>
+                <p>To interact with a Aire cluster, we will use its command-line interface, also known as the ultimate CLI. We use the command-line because it is the first place the AireCoin core team deploys new functionality. The command-line interface is not necessarily the easiest to use, but it provides the most direct, flexible, and secure access to your Solana accounts.</p>
                 <h3>This is the second heading</h3>
                 </>
                 <>
