9f58a36eac3f32ca1ab4298c06cdfa47d107e0db
Haseeb Mohammed <you@example.com>
2022-08-19 14:51:59 +0100

    added responsive

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import Nav from "./Components/Nav";
 import { Routes, Route } from "react-router-dom";
 import Nft from "./Components/routes/Nft";
 import Docs from "./Components/routes/Docs";
+import { useState } from "react";
 
 function App() {
+  const [respNav, setRespNav] = useState(false);
   return (
     <div className="App">
-      <Nav />
+      <Nav respNav={respNav} setRespNav={setRespNav} />
       <Routes>
-        <Route path="/" element={<Main />} />
+          <Route path="/" element={<Main />} />
         <Route path="NFT" element={<Nft />} />
         <Route path="Docs" element={<Docs />} />
       </Routes>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,9 +1,14 @@
-import React from "react";
-import { RiLayout4Fill } from "react-icons/ri";
+import { useState } from "react";
+import {
+  RiLayout4Fill,
+  RiBarChartHorizontalFill,
+  RiCloseLine,
+} from "react-icons/ri";
 import { Link } from "react-router-dom";
 import "./Nav.css";
 
-function Nav() {
+function Nav(props) {
+console.log(props.respNav)
   return (
     <div className="nav">
       <Link to="/">
@@ -12,18 +17,26 @@ function Nav() {
           <h1 className="nav-logo">AireCoin</h1>
         </div>
       </Link>
-      <ul className="nav-list">
-        <li>
-          <Link to="/NFT">NFT</Link>
-        </li>
-        <li>
-          <Link to="/docs">Documentation</Link>
-        </li>
-        <li>
-          <Link to="/News">News</Link>
-        </li>
-      </ul>
-      <button className="nav-cta">Connect Wallet</button>
+      <div className="nav-right">
+        <ul className="nav-list">
+          <li>
+            <Link to="/NFT">NFT</Link>
+          </li>
+          <li>
+            <Link to="/docs">Documentation</Link>
+          </li>
+          <li>
+            <Link to="/News">News</Link>
+          </li>
+        </ul>
+        <button className="nav-cta">Connect Wallet</button>
+        <button className="nav-btn nav--close-btn" onClick={() => props.setRespNav(false)}>
+          <RiCloseLine />
+        </button>
+      </div>
+      <button className="nav-btn" onClick={() => props.setRespNav(true)}>
+        <RiBarChartHorizontalFill />
+      </button>
     </div>
   );
 }
